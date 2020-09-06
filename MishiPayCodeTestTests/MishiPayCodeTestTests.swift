b63924784c6654db73b2feca0ac39442ad3ef001
Shyam Bhudia <shyambhudia@gmail.com>
2020-09-06 12:38:47 +0100

    Initial Commit

---
+++ MishiPayCodeTestTests/MishiPayCodeTestTests.swift
@@ -0,0 +1,34 @@
+//
+//  MishiPayCodeTestTests.swift
+//  MishiPayCodeTestTests
+//
+//  Created by Shyam Bhudia on 06/09/2020.
+//  Copyright © 2020 Shyam Bhudia. All rights reserved.
+//
+
+import XCTest
+@testable import MishiPayCodeTest
+
+class MishiPayCodeTestTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
